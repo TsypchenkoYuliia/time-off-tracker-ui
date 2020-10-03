c9edf3d4cf06c2145f4fb158e83567f0a210a3f6
Dmitro Krivovichev <angeldemon90@mail.ru>
2020-10-03 13:11:40 +0300

    Project created and all required packages installed with some settings

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import './scss/app.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+serviceWorker.unregister();
