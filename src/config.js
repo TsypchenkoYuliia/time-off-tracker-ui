d5f9d9580f905d7af51bfa42dd14a67df72712c0
Dmytro Kryvovychev <nondemondias4x@gmail.com>
2020-10-20 18:10:18 +0300

    Some changes in config for future interceptors

--- src/config.js
+++ src/config.js
@@ -1,1 +1,15 @@
-export const BASE_URL = 'http://localhost:3001/';
+import axios from 'axios';
+import { Context } from './Context';
+
+const BASE_URL = 'http://localhost:3001/';
+
+const axiosApi = axios.create({
+  baseURL: BASE_URL,
+});
+
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${Context.token}`;
+  return config;
+});
+
+export { BASE_URL, axiosApi };
